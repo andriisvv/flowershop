aa65897c60e49882beeb773732f1505d9c143daf
andriisvv <andriy.sopovskyi@kpk-lp.com.ua>
2026-04-01 16:15:23 +0300

    Add category filter in catalog

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -10,9 +10,16 @@ class CatalogController extends Controller
     public function index()
     {
         $categories = Category::withCount('products')->get();
-        $products = Product::with('category')
-                           ->where('is_active', true)
-                           ->paginate(6);
+
+        $query = Product::with('category')->where('is_active', true);
+
+        if (request('category')) {
+            $query->whereHas('category', function($q) {
+                $q->where('slug', request('category'));
+            });
+        }
+
+        $products = $query->paginate(6);
 
         return view('catalog.index', compact('categories', 'products'));
     }
